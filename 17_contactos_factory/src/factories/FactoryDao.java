f9a9f62d3d6533db2960b42da922a982c7574bb5
tarde <tarde@G24-EQ15>
2019-09-30 18:28:25 +0200

    commit 2

--- 17_contactos_factory/src/factories/FactoryDao.java
+++ 17_contactos_factory/src/factories/FactoryDao.java
@@ -3,6 +3,8 @@ package factories;
 import daos.DaoContactos;
 import daos.DaoContactosImpl;
 
+// Clase de factoría
+
 public class FactoryDao {
 	public static DaoContactos getDaoContactos() {
 		return new DaoContactosImpl();
